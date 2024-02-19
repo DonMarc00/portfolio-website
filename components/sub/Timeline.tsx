64c948062d58aec53580ea701385a0cb3b1a8bd8
Marc Novak <marc.novak1@web.de>
2024-02-19 22:06:24 +0100

    Added glow effect on eventcards

--- components/sub/Timeline.tsx
+++ components/sub/Timeline.tsx
@@ -1,30 +1,94 @@
 "use client"
-import React, { Fragment } from 'react'
-import '../../app/timeline.css';
-import {
-  slideInFromLeft,
-  slideInFromRight,
-  slideInFromTop,
-} from "@/utils/motion";
+import React, { Fragment, useEffect, useRef } from 'react'
 import { motion } from "framer-motion";
 
+type TimelineProps = {
+  events: Event[];
+};
+
+type EventCardProps = {
+  heading: string;
+  subHeading: string;
+  direction: 'left' | 'right';
+};
+
+type Event = {
+  heading: string;
+  subHeading: string;
+  direction: 'left' | 'right';
+};
+
 const Timeline = ({events}: TimelineProps) => {
+
+  useEffect(() => {
+    const cardsElement = document.getElementById("cards");
+    if (cardsElement) {
+      cardsElement.onmousemove = e => {
+        for(const card of document.getElementsByClassName("event-card")) {
+          const rect = card.getBoundingClientRect(),
+                x = e.clientX - rect.left,
+                y = e.clientY - rect.top;
+                console.log(x, y);
+
+          (card as HTMLElement).style.setProperty("--mouse-x", `${x}px`);
+          (card as HTMLElement).style.setProperty("--mouse-y", `${y}px`);
+        };
+      }
+    }
+  }, []);
+
+  const Circle = () => {
+    return (
+      <motion.div 
+        className='bg-gradient-to-r from-blue-500 to-teal-500 rounded-full w-4 h-4 mx-auto'
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+      ></motion.div>
+    )
+  }
+  
+  const EventCard = ({heading, subHeading, direction}: EventCardProps) => {
+
+    return (
+      <motion.div className='event-card flex flex-col gap-y-2 shadow-white rounded-lx p-4 z-[30] '
+      initial={{ opacity: 0, x: direction === 'left' ? -100 : 100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.5 }}>
+          <div className="card-content">
+        <div className='text-white font-bold text-lg border-b'>{heading}</div>
+        <div className='text-sm text-gray-700'>{subHeading}</div></div>
+      </motion.div>
+    )
+  }
+  
+  const Pillar = () => {
+    return (
+      <motion.div 
+        className='bg-gradient-to-b from-blue-500 to-teal-500 rounded-t-full rounded-b-full w-2 h-full mx-auto'
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+      ></motion.div>
+    )
+  }
+
   return (
-    <div className='flex flex-col gap-y-3 w-full my-4'>
+    <div className='cards flex flex-col gap-y-3 w-full my-4' id="cards">
       <Circle />
       {events.map((event, index) => {
         return (
           <Fragment key={index}>
-            <div className='grid grid-cols-[1fr_auto_1fr] gap-x-2 items-center mx-auto'>
+            <div className=' grid grid-cols-[1fr_auto_1fr] gap-x-2 items-center mx-auto'>
               {event.direction === 'left' ? (
-                <EventCard heading={event.heading} subHeading={event.subHeading} />
+                <EventCard heading={event.heading} subHeading={event.subHeading} direction='left'/>
               ) :
               (
                 <div></div>
               )}
               <Pillar />
               {event.direction === 'right' ? (
-                <EventCard heading={event.heading} subHeading={event.subHeading} />
+                <EventCard heading={event.heading} subHeading={event.subHeading} direction='right'/>
               ) :
               (
                 <div></div>
@@ -39,40 +103,5 @@ const Timeline = ({events}: TimelineProps) => {
   )
 }
 
-const Circle = () => {
-  return (
-    <div className='bg-gradient-to-r from-blue-500 to-teal-500 rounded-full w-4 h-4 mx-auto'></div>
-  )
-}
-
-const Pillar = () => {
-  return (
-    <div className='bg-gradient-to-b from-blue-500 to-teal-500 rounded-t-full rounded-b-full w-2 h-full mx-auto'></div>
-  )
-}
-
-type EventCardProps = {
-  heading: string;
-  subHeading: string;
-};
-
-type TimelineProps = {
-  events: Event[];
-};
-
-type Event = {
-  heading: string;
-  subHeading: string;
-  direction: 'left' | 'right';
-};
-
-const EventCard = ({heading, subHeading}: EventCardProps) => {
-  return (
-    <div className='transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-2xl flex flex-col gap-y-2 border shadow-md shadow-white rounded-lx p-4'>
-      <div className='text-white font-bold text-lg border-b'>{heading}</div>
-      <div className='text-sm text-gray-700'>{subHeading}</div>
-    </div>
-  )
-}
 
 export default Timeline
